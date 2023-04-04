1c78a8dce7753098ff468ec1e39fc66971c3a438
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-04-04 20:49:44 +0500

    Add function to create file with BufWriter

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader, BufWriter};
+use std::os::unix::fs::OpenOptionsExt;
 
 #[allow(dead_code)]
 fn create_file_bufreader(file_path: &str) -> Result<BufReader<File>, Box<dyn Error>> {
@@ -21,4 +23,16 @@ fn find_line_in_file_bufreader(line_number: u32, file_bufreader: BufReader<File>
         line_counter += 1;
     }
     found_line
+}
+
+#[allow(dead_code)]
+fn create_script_file_bufwriter() -> Result<BufWriter<File>, Box<dyn Error>> {
+    let script_file_name = "script_by_sheller.sh";
+    let script_file_options = OpenOptions::new()
+        .append(true)
+        .create_new(true)
+        .mode(0o744)
+        .open(script_file_name)?;
+    let script_file_bufwriter = BufWriter::new(script_file_options);
+    Ok(script_file_bufwriter)
 }
