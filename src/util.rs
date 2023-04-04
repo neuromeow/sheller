50572e993e753aa659405d047e9a3dce33503c81
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-04-04 20:37:20 +0500

    Add function to find line in file BufReader

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,24 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 
 #[allow(dead_code)]
 fn create_file_bufreader(file_path: &str) -> Result<BufReader<File>, Box<dyn Error>> {
     let file = File::open(file_path)?;
     let file_bufreader = BufReader::new(file);
     Ok(file_bufreader)
+}
+
+#[allow(dead_code)]
+fn find_line_in_file_bufreader(line_number: u32, file_bufreader: BufReader<File>) -> Option<String> {
+    let mut found_line: Option<String> = None;
+    let mut line_counter: u32 = 1;
+    for line in file_bufreader.lines() {
+        if line_counter == line_number {
+            found_line = Some(line.unwrap());
+            break
+        }
+        line_counter += 1;
+    }
+    found_line
 }
