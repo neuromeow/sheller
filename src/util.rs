b5e1b91bd3faeb857d3fcd07282a4dd4564835ae
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-04-05 21:33:23 +0500

    Reformat code with rustfmt

--- src/util.rs
+++ src/util.rs
@@ -11,13 +11,16 @@ fn create_file_bufreader(file_path: &OsString) -> Result<BufReader<File>, Box<dy
     Ok(file_bufreader)
 }
 
-fn find_line_in_file_bufreader(line_number: u32, file_bufreader: BufReader<File>) -> Option<String> {
+fn find_line_in_file_bufreader(
+    line_number: u32,
+    file_bufreader: BufReader<File>,
+) -> Option<String> {
     let mut found_line: Option<String> = None;
     let mut line_counter: u32 = 1;
     for line in file_bufreader.lines() {
         if line_counter == line_number {
             found_line = Some(line.unwrap());
-            break
+            break;
         }
         line_counter += 1;
     }
@@ -35,14 +38,20 @@ fn create_script_file_bufwriter() -> Result<BufWriter<File>, Box<dyn Error>> {
     Ok(script_file_bufwriter)
 }
 
-fn update_script_file_bufreader(body: String, file_bufwriter: &mut BufWriter<File>) -> Result<(), Box<dyn Error>> {
+fn update_script_file_bufreader(
+    body: String,
+    file_bufwriter: &mut BufWriter<File>,
+) -> Result<(), Box<dyn Error>> {
     let script_header = String::from("#!/bin/bash\n") + "#\n" + "# Script Description\n\n";
     let script_content = script_header + &body + "\n";
     file_bufwriter.write_all(script_content.as_bytes())?;
     Ok(())
 }
 
-pub fn build_script_file(line_number: u32, history_file_path: &OsString) -> Result<(), Box<dyn Error>> {
+pub fn build_script_file(
+    line_number: u32,
+    history_file_path: &OsString,
+) -> Result<(), Box<dyn Error>> {
     let history_file_bufreader = create_file_bufreader(history_file_path)?;
     println!("Starting the script build process...");
     println!("The history file you passed: {:?}", history_file_path);
@@ -52,11 +61,11 @@ pub fn build_script_file(line_number: u32, history_file_path: &OsString) -> Resu
             let mut script_file_bufwriter = create_script_file_bufwriter()?;
             update_script_file_bufreader(command, &mut script_file_bufwriter)?;
             println!("Your script has been created!");
-        },
+        }
         _ => {
             println!("The specified history file doesn't contain a command with the given number.");
             std::process::exit(1);
-        },
+        }
     }
     Ok(())
 }
