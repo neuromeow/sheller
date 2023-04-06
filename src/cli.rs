90e542f7283df13c4ac0d8664079b907ddecc909
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-04-06 21:58:02 +0500

    Add function to parse future passed arguments

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,5 @@
 use std::ffi::OsString;
+use std::ops::Range;
 
 use clap::{Parser, Subcommand};
 
@@ -16,4 +17,14 @@ pub enum Commands {
         #[arg(short, long, required = true)]
         line: u32,
     },
+}
+
+#[allow(dead_code)]
+fn parse_passed_lines(line_number_or_range: &str) -> Result<Range<u32>, std::num::ParseIntError> {
+    let mut line_number_or_range_split = line_number_or_range.split("..");
+    let line_range_start = line_number_or_range_split.next().unwrap();
+    let line_range_start_parsed = line_range_start.parse::<u32>()?;
+    let line_range_end = line_number_or_range_split.last().unwrap_or(line_range_start);
+    let line_range_end_parsed = line_range_end.parse::<u32>()?;
+    Ok(line_range_start_parsed..line_range_end_parsed)
 }
