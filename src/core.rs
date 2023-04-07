5d0db2e36be7e8a531a2964cfa576d520074bf06
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-04-07 19:24:18 +0500

    Remove the old code that builds a script with one line

--- src/core.rs
+++ src/core.rs
@@ -3,7 +3,6 @@ use std::error::Error;
 use clap::Parser;
 
 use crate::cli::{Cli, Commands};
-// use crate::util::build_script_file;
 use crate::util::build_script_file_with_multiple_line_ranges;
 use crate::util::print_passed_parameters;
 
@@ -12,7 +11,6 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     match &cli.command {
         Commands::Build { history_file, lines } => {
             print_passed_parameters(lines, history_file)?;
-            // build_script_file(*line, history_file)?;
             build_script_file_with_multiple_line_ranges(lines, history_file)?;
         }
     }
--- src/util.rs
+++ src/util.rs
@@ -13,20 +13,25 @@ fn create_file_bufreader(file_path: &OsString) -> Result<BufReader<File>, Box<dy
     Ok(file_bufreader)
 }
 
-fn find_line_in_file_bufreader(
-    line_number: u32,
-    file_bufreader: BufReader<File>,
-) -> Option<String> {
-    let mut found_line: Option<String> = None;
-    let mut line_counter: u32 = 1;
-    for line in file_bufreader.lines() {
-        if line_counter == line_number {
-            found_line = Some(line.unwrap());
-            break;
+fn create_script_file_bufwriter() -> Result<BufWriter<File>, Box<dyn Error>> {
+    let script_file_name = "script_by_sheller.sh";
+    let script_file_options = OpenOptions::new()
+        .append(true)
+        .create_new(true)
+        .mode(0o744)
+        .open(script_file_name)?;
+    let script_file_bufwriter = BufWriter::new(script_file_options);
+    Ok(script_file_bufwriter)
+}
+
+fn create_hashmap_from_ranges_vector(ranges_vector: &Vec<Range<u32>>) -> HashMap<u32, Option<String>> {
+    let mut result = HashMap::new();
+    for range in ranges_vector {
+        for number in range.clone() {
+            result.insert(number, None);
         }
-        line_counter += 1;
     }
-    found_line
+    result
 }
 
 fn update_lines_hashmap_by_file_bufreader_content(
@@ -41,38 +46,6 @@ fn update_lines_hashmap_by_file_bufreader_content(
     }
 }
 
-fn create_script_file_bufwriter() -> Result<BufWriter<File>, Box<dyn Error>> {
-    let script_file_name = "script_by_sheller.sh";
-    let script_file_options = OpenOptions::new()
-        .append(true)
-        .create_new(true)
-        .mode(0o744)
-        .open(script_file_name)?;
-    let script_file_bufwriter = BufWriter::new(script_file_options);
-    Ok(script_file_bufwriter)
-}
-
-fn update_script_file_bufreader(
-    body: String,
-    file_bufwriter: &mut BufWriter<File>,
-) -> Result<(), Box<dyn Error>> {
-    let script_header = String::from("#!/bin/bash\n") + "#\n" + "# Script Description\n\n";
-    let script_content = script_header + &body + "\n";
-    file_bufwriter.write_all(script_content.as_bytes())?;
-    Ok(())
-}
-
-#[allow(dead_code)]
-fn create_hashmap_from_ranges_vector(ranges_vector: &Vec<Range<u32>>) -> HashMap<u32, Option<String>> {
-    let mut result = HashMap::new();
-    for range in ranges_vector {
-        for number in range.clone() {
-            result.insert(number, None);
-        }
-    }
-    result
-}
-
 fn update_script_file_bufreader_by_lines_hashmap(
     ranges_vector: &Vec<Range<u32>>,
     lines_hashmap: HashMap<u32, Option<String>>,
@@ -85,26 +58,6 @@ fn update_script_file_bufreader_by_lines_hashmap(
             let command = lines_hashmap.get(&number).clone().unwrap();
             let command = command.clone().unwrap() + "\n";
             file_bufwriter.write_all(command.as_bytes())?;
-        }
-    }
-    Ok(())
-}
-
-#[allow(dead_code)]
-pub fn build_script_file(
-    line_number: u32,
-    history_file_path: &OsString,
-) -> Result<(), Box<dyn Error>> {
-    let history_file_bufreader = create_file_bufreader(history_file_path)?;
-    match find_line_in_file_bufreader(line_number, history_file_bufreader) {
-        Some(command) => {
-            let mut script_file_bufwriter = create_script_file_bufwriter()?;
-            update_script_file_bufreader(command, &mut script_file_bufwriter)?;
-            println!("Your script has been created!");
-        }
-        _ => {
-            println!("The specified history file doesn't contain a command with the given number.");
-            std::process::exit(1);
         }
     }
     Ok(())
