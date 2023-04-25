f19d721bec23f901542a654ecdfa8ba6722b42b3
neuromeow <92692649+neuromeow@users.noreply.github.com>
2023-04-25 19:05:20 +0500

    Add simple unit tests for function to parse lines option arguments

--- src/cli.rs
+++ src/cli.rs
@@ -71,13 +71,48 @@ fn parse_specified_lines(
 ) -> Result<Range<u32>, std::num::ParseIntError> {
     if line_number_or_lines_range.contains("..") {
         let mut line_range_split = line_number_or_lines_range.split("..");
-        let lines_range_start = line_range_split.next().unwrap();
-        let lines_range_start_parsed = lines_range_start.parse::<u32>()?;
-        let lines_range_end = line_range_split.last().unwrap();
-        let lines_range_end_parsed = lines_range_end.parse::<u32>()?;
+        let lines_range_start_parsed = line_range_split.next().unwrap().parse::<u32>()?;
+        let lines_range_end_parsed = line_range_split.last().unwrap().parse::<u32>()?;
         Ok(lines_range_start_parsed..lines_range_end_parsed)
     } else {
         let line_number_parsed = line_number_or_lines_range.parse::<u32>()?;
         Ok(line_number_parsed..line_number_parsed + 1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_specified_lines_should_parse_correctly() {
+        let values = [
+            ("0", 0..1),
+            ("0..1", 0..1),
+            ("0..3", 0..3),
+            ("1..0", 1..0),
+            ("2..0", 2..0),
+            ("1", 1..2),
+            ("1..2", 1..2),
+            ("1..5", 1..5),
+            ("11..24", 11..24),
+            ("2..1", 2..1),
+            ("6..1", 6..1),
+            ("18..7", 18..7),
+        ];
+        for (input_value, expected_value) in values {
+            let actual_value = parse_specified_lines(input_value).unwrap();
+            assert_eq!(actual_value, expected_value);
+        }
+    }
+
+    #[test]
+    fn parse_specified_returns_error_when_string_cannot_be_parsed() {
+        let values = [
+            "zero", "1..", "..5", "12..", "..", " 6", "9 ", " 17 ", " ", "",
+        ];
+        for value in values {
+            assert!(parse_specified_lines(value).is_err());
+        }
     }
 }
